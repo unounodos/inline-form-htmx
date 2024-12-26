81b75ae8981a99d6951d19d652f59b66c3d29aec
Paul Rijke <paul@ibuildings.nl>
2024-12-26 13:06:41 +0100

    link naar session for events

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,5 +13,8 @@ class HelloController
 //        dd($request->all());
         return view('welcome', ['name' => $request->input('name')])
             ->with('event', 'name:provided');
+        // we kunnen de sessie gebruiken om events te js triggeren
+        // deze truc zag ik in https://youtu.be/vNiZyFVmoOI?t=1393
+
     }
 }
