0a18a814c25773641b9ff44bbc5a8ab05cdff364
Paul Rijke <paul@ibuildings.nl>
2025-01-18 16:20:50 +0100

    Redirect to profile2

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 //
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return redirect('/profile2');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     // routes/web.php
